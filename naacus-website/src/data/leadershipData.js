580fc711e1032ad99a085c7b5280f20d816455aa
Celestin Mbuyamba <celembuyamba@gmail.com>
2026-01-25 21:00:15 -0600

    Add photo for President Emeritus Ntal Alimasi

--- naacus-website/src/data/leadershipData.js
+++ naacus-website/src/data/leadershipData.js
@@ -136,7 +136,7 @@ export const leadershipData = {
       title: 'President Emeritus', 
       phone: '202-256-1781', 
       email: 'ntal.alimasi@naacus.org', 
-      photo: '',
+      photo: '/images/leadership/president-emeritus.png',
       bio: 'Ntal Alimasi is one of the Association founding team members and its first President. He is a former Social Secretary of ACA-DMV and currently co-chairs the NAACUS\' Advocacy arm.\n\nA cradle Catholic from the DRCongo in a family that boasts an uncle priest, an aunt and sister nuns, husband and father, Ntal is a parishioner of Saint Augustine Parish in Washington, DC.\n\nNtal Alimasi was one of the speakers at the Convocation of Catholic Leaders (Orlando 2017). Known for his work in promoting social justice and community engagement within the Catholic Church, his contributions often emphasize the importance of inclusivity and outreach to marginalized communities.\n\nOutside the Church, Ntal Alimasi is an independent international development consultant with proven expertise in governance and anti-corruption, social and economic community development, and African diaspora networks. He has worked with the World Bank Group, the African Development Bank, USAID, DFID, US Department of State, Belgian Cooperation, UNDP, and various other international organizations and foundations.\n\nNtal speaks English, French, Swahili, Lingala, Mashi, Kinyarwanda, Kirundi, Kikongo, and reads Latin.'
     },
   ]
